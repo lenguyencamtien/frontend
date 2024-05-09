a57f91fa635f42597a1f4709cd87bd127a74b233
lenguyencamtien <camtien20302@gmail.com>
2024-05-09 17:28:20 +0700

    p27

---
+++ src/components/Following.js
@@ -0,0 +1,14 @@
+import React from "react";
+import FollowingCard from "./FollowingCard";
+
+const Following = ({userData, profile, auth, id}) =>{
+    console.log({userData})
+    return(
+        <div>
+            {userData.length > 0 && userData.map(user => (
+                <FollowingCard user={user.following}/>
+            ))}
+        </div>
+    )
+}
+export default Following;
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -8,9 +8,46 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getProfileUsers } from "../redux/actions/profileActions";
 import {useEffect, useState} from "react";
+import { IconButton } from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import PeopleIcon from '@mui/icons-material/People';
+import BookmarksIcon from '@mui/icons-material/Bookmarks';
+import Friends from '../components/Friends';
+import Following from '../components/Following';
 
 const Profile = () => {
     const [userData, setUserData] = useState([]);
+
+const [showaccount, setshowaccount] = useState(true);
+const [showfriends, setshowfriends] = useState(false);
+const [showfollowing, setshowfollowing] = useState(false);
+const [showsaved, setshowsaved] = useState(false);
+const handletoggle = (ht) => {
+    
+    if(ht === 'showaccount'){
+        setshowsaved(false);
+        setshowfriends(false);
+        setshowfollowing(false);
+        setshowaccount(true)
+    }else if(ht === 'showfriends'){
+        setshowsaved(false);
+        setshowfriends(true);
+        setshowfollowing(false);
+        setshowaccount(false)
+    }else if(ht === 'showfollowing'){
+        setshowsaved(false);
+        setshowfriends(false);
+        setshowfollowing(true);
+        setshowaccount(false)
+    }else if(ht === 'showsaved'){
+        setshowsaved(true);
+        setshowfriends(false);
+        setshowfollowing(false);
+        setshowaccount(false)
+    }
+}
+
     const {id} = useParams();
     const {auth, profile} = useSelector(state => state);
     const dispatch = useDispatch();
@@ -26,6 +63,26 @@ const Profile = () => {
     return (
         <div className='profile'>
             <Info userData={userData} profile={profile} auth={auth} id={id}/>
+            <div className='profileheader'>
+                <div className='profileheader-items'>
+                    <IconButton className='profileheader-item' onClick={()=>handletoggle('showaccount')}>
+                        <AccountCircleIcon/>
+                    </IconButton>
+                    <hr/>
+                    <IconButton onClick={()=>handletoggle('showfriends')}>
+                        <PeopleIcon/>
+                    </IconButton>
+                    <hr/>
+                    <IconButton onClick={()=>handletoggle('showfollowing')}>
+                        <PersonAddIcon/>
+                    </IconButton>
+                    <hr/>
+                    <IconButton onClick={()=>handletoggle('showsaved')}>
+                        <BookmarksIcon/>
+                    </IconButton>
+                </div>
+            </div>
+            { showaccount && 
             <div className='profilebody'>
                 <div className='profilebody-left'>
             <About userData={userData} profile={profile} auth={auth} id={id}/>
@@ -37,6 +94,16 @@ const Profile = () => {
             <Posts/>
                 </div>
             </div>
+        }
+        {
+            showfriends && <Friends userData={userData} profile={profile} auth={auth} id={id}/>
+        }
+        {
+            showfollowing && <Following userData={userData} profile={profile} auth={auth} id={id}/>
+        }
+        {
+            showsaved && <h3>Saved</h3>
+        }
         </div>
     )
 }
